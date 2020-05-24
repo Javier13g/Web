56ec0deb1f95cbd69eedc15530f9967fcd7dd6c5
Javier13g <61069624+Javier13g@users.noreply.github.com>
2020-05-24 01:42:07 -0400

    Mas avance

--- Agenda/Js/index.js
+++ Agenda/Js/index.js
@@ -1,9 +1,57 @@
-function gi(id) 
+//VARIABLE QUE ALMACENA LAS INSTANCIAS DE CONTACTOS
+var contactos = new Array();
+//ESTO DEFINE EL OBJETO CONTACTO CON SUS RESPECTIVAS PROPIEDADES
+function contacto() 
 {
-     return document.getElementById(id);
+    this.nombre ="";
+    this.apellido = "";
+    this.telefono = "";
 }
 
-function agregarContacto(params) 
+con = new contacto();
+con.nombre
+
+//FUNCION PARA ABREVIAR document.getElementByID
+function gi(id) 
+{
+    return document.getElementById(id);
+}
+//FUNCION QUE MUESTRA EL FORMULARIO PARA PODER AGREGAR
+function agregarContacto() 
 {
     gi("divFormulario").style.display='inline';
+}
+//FUNCION QUE OCULTA EL FORMULARIO
+function ocultarForm() 
+{
+    gi('txtNombre').value = '';
+    gi('txtApellido').value = '';
+    gi('txtTelefono').value = '';
+    gi("divFormulario").style.display='none';  
+}
+//FUNCION QUE GUARDA LOS DATOS Y AL TERMINAR OCULTA EL FORMULARIO
+function guardar() 
+{
+    con = new contacto();
+    con.nombre = gi('txtNombre').value;
+    con.apellido = gi('txtApellido').value;
+    con.telefono = gi('txtTelefono').value;
+    contactos[contactos.length] = con;
+    ocultarForm();
+    mostrarContactos();
+}
+
+function mostrarContactos() 
+{
+    for(x=0; x<contactos.length; x++) 
+    {
+        con = contactos[x];
+        div = document.createElement('div');
+        div.setAttribute('class','contacto');
+        div.innerHTML = 
+        con.nombre+"<br/>"+
+        con.apellido+"<br/>"+
+        con.telefono+"<br/>";
+        gi('todosLosContactos').appendChild(div);
+    }    
 }
