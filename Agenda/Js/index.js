6dd3ed90316ba641e1b86b4b2d307eaf475881c5
Javier13g <61069624+Javier13g@users.noreply.github.com>
2020-05-27 19:34:46 -0400

    casi terminando

--- Agenda/Js/index.js
+++ Agenda/Js/index.js
@@ -1,5 +1,5 @@
 
-function addCon()
+/* function addCon()
 {
  
     var InputtxtTipoContacto = document.getElementById("txtTipoContacto");
@@ -14,18 +14,13 @@ function addCon()
     var InputtxtTelefono = document.getElementById("txtTelefono");
     var telefono = InputtxtTelefono.value
 
-    alert(nombre);
+    document.getElementById("divFormulario").style.display='none';
     console.log(nombre);
-    alert(apellido);
-    alert(telefono);
-    alert(tipo);
-   
-}
-function agregarContacto() 
-{
-    document.getElementById("divFormulario").style.display='inline';
-}
-/*
+    console.log(apellido);
+    console.log(telefono);
+    console.log(tipo);   
+} */
+
 //VARIABLE QUE ALMACENA LAS INSTANCIAS DE CONTACTOS
 var contactos = new Array();
 //ESTO DEFINE EL OBJETO CONTACTO CON SUS RESPECTIVAS PROPIEDADES
@@ -34,13 +29,14 @@ function contacto()
     this.nombre = "";
     this.apellido = "";
     this.telefono = "";
+    this.tipoTelefono = "";
 }
-
 
 //FUNCION QUE MUESTRA EL FORMULARIO PARA PODER AGREGAR
 function agregarContacto() 
 {
     document.getElementById("divFormulario").style.display='inline';
+    document.getElementById('txtNombre').focus();
 }
 //FUNCION QUE OCULTA EL FORMULARIO
 function ocultarForm() 
@@ -48,6 +44,7 @@ function ocultarForm()
     document.getElementById('txtNombre').value = '';
     document.getElementById('txtApellido').value = '';
     document.getElementById('txtTelefono').value = '';
+    document.getElementById('txtTipoContacto')
     document.getElementById("divFormulario").style.display='none';  
 }
 //FUNCION QUE GUARDA LOS DATOS Y AL TERMINAR OCULTA EL FORMULARIO
@@ -57,24 +54,41 @@ function guardar()
     con.nombre = document.getElementById('txtNombre').value;
     con.apellido = document.getElementById('txtApellido').value;
     con.telefono = document.getElementById('txtTelefono').value;
-    contactos[contactos.length] = con;
+    con.tipoTelefono = document.getElementById('txtTipoContacto').value;
+    contactos[contactos.length] = con; 
     ocultarForm();
     mostrarContactos();
 }
 
+function eliminarContacto(id_contacto)
+{
+    if(confirm("Quiere eliminar el contacto?"))
+    {
+        newCon = new Array();
+        for(x=0; x<contactos.length; x++)
+        {
+            if(x != id_contacto)
+            {
+                newCon[newCon.length] = contactos[x];
+            }
+        }
+        contactos = newCon;
+        mostrarContactos();
+    }
+}
+//FUNCION QUE MUESTRA LOS CONTACTOS
 function mostrarContactos() 
 {
-    alert("contacto registrado")
-    for(x=0; x<contactos.length; x++) 
+    for(x=0; x<contactos.length; x++);
     {
-        con = contactos[x];
         div = document.createElement('div');
-        div.setAttribute('class','contacto');
-        div.innerHTML = 
-        con.nombre+"<br/>"+
-        con.apellido+"<br/>"+
-        con.telefono+"<br/>";
+        div.setAttribute('class','contacto'); 
+        div.innerHTML =
+        "Nombre: "+ con.nombre + "<br/>"+
+        "Apellido: "+ con.apellido + "<br/>"+
+        "Tipo de contacto: "+ con.tipoTelefono + "<br/>"+
+        "Telefono: "+ con.telefono + "<br/>"+
+        "<button onclick='editarContacto("+x+")' class='btn btn-primary' id='botones'>Editar</button><button onclick='eliminarContacto("+x+")' class='btn btn-warning' id='botones'>Eliminar</button>";
         document.getElementById('todosLosContactos').appendChild(div);
-    }    
+    }
 } 
-*/
